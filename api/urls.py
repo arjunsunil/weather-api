2139ff07945dc8655038f0f88b9895becf12c5b7
Arjun <arjun.pp@jiffy.ai>
2021-06-07 11:46:28 +0530

    updated the api urls endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from .views import SendEmail, UserListView, ListWeather
 from django.urls import path, include
 
 urlpatterns = [
-    path('weather/', ListWeather.as_view(), name='login'),
+    path('weather/list/', ListWeather.as_view(), name='login'),
     path('users/', UserListView.as_view()),
-    path('email/', SendEmail.as_view(), name='login'),
+    path('send/email/', SendEmail.as_view(), name='login'),
     path('auth/', include('rest_auth.urls'))
 ]
